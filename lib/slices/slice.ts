2cba9e5087fe5db8813d15e0606e839c583295f1
Joshua Oloton <olotonjoshua@gmail.com>
2025-03-15 15:08:18 +0100

    feat: Set up Redux Toolkit for global state management
    feat: Configured store, added slices, and integrated with the app.

---
+++ lib/slices/slice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface FiltersState {
+  selectedFilters: string[];
+}
+
+const initialState: FiltersState = {
+  selectedFilters: [],
+};
+
+const filterSlice = createSlice({
+  name: "filters",
+  initialState,
+  reducers: {
+    addFilter: (state, action: PayloadAction<string>) => {
+      if (state.selectedFilters.includes(action.payload)) {
+        state.selectedFilters.splice(state.selectedFilters.indexOf(action.payload), 1);
+        return;
+      }
+      state.selectedFilters.push(action.payload);
+    },
+
+    addSingleFilter: (state, action: PayloadAction<string>) => {
+      state.selectedFilters = []; // clear all filters
+      state.selectedFilters.push(action.payload);
+    },
+
+    removeFilter: (state, action: PayloadAction<string>) => {
+      state.selectedFilters = state.selectedFilters.filter((filter) => filter !== action.payload);
+    },
+
+    clearFilters: (state) => {
+      state.selectedFilters = [];
+    }
+  },
+});
+
+export const { addFilter, addSingleFilter, removeFilter, clearFilters } = filterSlice.actions;
+export default filterSlice.reducer;
---
+++ lib/store.ts
@@ -0,0 +1,31 @@
+import { configureStore } from "@reduxjs/toolkit";
+import filtersReducer from "@/lib/slices/slice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, filtersReducer);
+
+export const store = configureStore({
+  reducer: {
+    filters: persistedReducer,
+  },
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
