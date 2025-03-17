a8ef146780ded9046dd5743eecbe4307ecadad67
Joshua Oloton <olotonjoshua@gmail.com>
2025-03-17 12:20:58 +0100

    feat: add footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <section className='h-52 bg-gray-800 text-white flex justify-center items-center'>
+      <div className="text-center">
+        <h5 className='font-bold font-jost text-3xl my-4'>Daniel's Empire.</h5>
+        <p className="font-merriweather-sans">&copy;2025 E-commerce Application. All rights reserved</p>
+        <div className="flex flex-col md:flex-row gap-3"></div>
+      </div>
+    </section>
   )
 }
 
